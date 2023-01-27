ca7466543e2a911f74d78f740b4bff54ebf13f7b
c0de-slayer <fsali315@gmail.com>
2023-01-27 18:55:37 +0530

    edit view done delete not working

--- app.py
+++ app.py
@@ -40,6 +40,14 @@ with open('photo_data.json','r') as f:
 
 index = [1, 2, 3, 4, 5, 6, 4, 7, 4, 7, 8, 7, 9,2,10,11,12,10,13]
 
+def getting_category():
+    get_category = metadata.query.all()
+    unique_category = set([get_category[i].category for i in range(len(get_category))])
+    if len(unique_category) == 0:
+        unique_category = '0'
+    return unique_category
+    
+
 @app.route('/')
 def home():
     return render_template('index.html',photo_data=photo_data,range_len=len(photo_data),index=index)
@@ -48,7 +56,7 @@ def home():
 # @login_required
 def dashboard():
     metadata_query = metadata.query.all()
-    return render_template('dashboard.html',photo_data=metadata_query,range_len=len(metadata_query))
+    return render_template('dashboard.html',photo_data=metadata_query,range_len=len(metadata_query),catergory=getting_category(),category_len=len(getting_category()))
 
 
 @app.route('/login', methods=['GET','POST'])
@@ -78,7 +86,7 @@ def logout():
 @app.route('/upload', methods=['GET','POST'])
 def upload():
     if request.method == 'GET':
-        return render_template('upload.html')
+        return render_template('upload.html',catergory=getting_category() )
     else:
         title = request.form['title']
         sub_title = request.form['sub_title']
@@ -98,5 +106,34 @@ def upload():
         flash('Photo Updated Successfully')
         return redirect(url_for('upload'))
 
+
+@app.route('/update',methods=['POST'])
+def update():
+    if request.method == 'POST':
+
+
+        title = request.form['title']
+        sub_title = request.form['sub_title']
+        category = request.form['category']
+        id = request.form['ikivalue']       
+        photo_data = metadata.query.filter_by(id=id)
+        try:
+            path = request.files['file']
+            path1 = os.path.join(app.config['UPLOAD_FOLDER'],path.filename)
+            path.save(path1) 
+        except FileNotFoundError:
+            pass
+
+        metadata.query.filter_by(id=id).update(dict(title=title if title !='' else photo_data[0].title,sub_title=sub_title if sub_title !='' else photo_data[0].sub_title,category=category if category !='' else photo_data[0].category,photo_path=path1 if path !='' else photo_data[0].photo_path))
+        db.session.commit()
+
+        return redirect(url_for('dashboard'))
+        
+@app.route('/delete',methods=['POST'])
+def delete():
+    if request.method == 'POST':
+        id = request.form['ikivalue']
+        
+
 if __name__ == '__main__':
-    app.run(debug=True,host='192.168.0.107')
+    app.run(debug=True,host='172.25.171.70')

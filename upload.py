fc32bf76078025d48203c7e8520b56ee48e716bd
C0DE-SLAYER <fsali315@gmail.com>
2023-01-28 00:44:27 +0530

    crud operation done

--- upload.py
+++ upload.py
@@ -1,19 +1,4 @@
-from flask import Flask, render_template, request, url_for
-from werkzeug.utils import secure_filename
+import os
 
 
-app = Flask(__name__)
-app.config['UPLOAD_FOLDER']
-
-	
-@app.route('/', methods = ['GET', 'POST'])
-def upload_file():
-    if request.method == 'GET':
-        return render_template('gd.html')
-    else: 
-        f = request.files['file']
-        f.save(secure_filename(f.filename))
-        return 'file uploaded successfully'
-		
-if __name__ == '__main__':
-   app.run(debug = True)
+os.remove('static/img/Screenshot from 2023-01-08 18-12-39.png')

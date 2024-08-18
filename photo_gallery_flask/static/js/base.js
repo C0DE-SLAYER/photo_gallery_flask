4bffd84003e4863f3b119563f1aec2804729bdb2
c0de-slayer <fsali315@gmail.com>
2024-08-18 18:07:55 +0530

    fix the flash card and added login button

--- photo_gallery_flask/static/js/base.js
+++ photo_gallery_flask/static/js/base.js
@@ -122,11 +122,8 @@ function previewImgs(event) {
 
     }
 }
-
 // flash message
 const close_btn = document.getElementById('to_close')
 setTimeout(() => {
-    close_btn.classList.remove('flash_show');
-    close_btn.classList.add('flash_hide');
-},4000);
-console.log(close_btn)
+  close_btn.remove()
+}, 4000);

ba5973123b7a3dea05374b7360772093d262dc61
Faisal Ali Sayyed <80758388+C0DE-SLAYER@users.noreply.github.com>
2024-08-18 19:02:18 +0530

    Bug fix: ist.remove(x): x not in list in file utils

--- photo_gallery_flask/utils.py
+++ photo_gallery_flask/utils.py
@@ -4,5 +4,10 @@ from photo_gallery_flask import db
 def getting_category():
     get_category = db.session.query(metadata.category).distinct().all()
     unique_category = [category[0] for category in get_category]
-    unique_category.remove('')
+    
+    try:
+        unique_category.remove('')
+    except Exception as e:
+        print(e)
+        
     return unique_category
